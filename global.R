38f6b2fbadca09c964e65cdf460e47e2764cb092
David Hall <125618886+davidphall@users.noreply.github.com>
2023-04-11 17:26:37 -0600

    Add files via upload

--- global.R
+++ global.R
@@ -1,9 +1,20 @@
 library(tidyverse)
+library(data.table)
 library(shiny)
 library(shinythemes)
 library(shinydashboard)
 library(plotly)
 library(bslib)
+library(feather)
+library(DT)
+
+
+load('data/unique_cpc_group.Rdata')
+assignee <- read_feather('data/g_assignee_disambiguated_2012_2021.feather')
+location <- read_feather('data/g_location_disambiguated_2012_2021.feather')
+patent <- read_feather('data/g_patent_2012_2021.feather')
+cpc <- fread('data/g_cpc_current_2012_2021.csv')
+cpc$patent_id <- as.character(cpc$patent_id)
 
 # Resources
   # https://shiny.rstudio.com/
@@ -12,20 +23,19 @@ library(bslib)
   # HTML tags https://shiny.rstudio.com/articles/tag-glossary.html
   # Inputs https://shiny.rstudio.com/tutorial/written-tutorial/lesson3/
 
-
 # Agenda:
-# UI
-  # Layout: Fluidpage, Fluidrow, Columns, navbarpage, shiny dashboard
-    # Look/feel: html, css, bslib, shiny themes
-  # Inputs and outputs
-
-
-  # Work together in class: 
-    # Design a UI that has the following
-      # A landing page and two analysis pages (competition and trends pages)
-        # Each analysis page needs a place for inputs and a place for outouts (charts)
-        # Inputs: 3 text inputs and 1 action button
-        # Outputs: blank for now, just give it a title
+  # Server
+    # Reactivity
+      # Reactive values
+      # Inputs
+      # Server logic
+      # Rendering outputs
+      # Displaying in UI
+    # Observers
+    # Debugging - browser()
+    # Work together in class:
+      # Add code from competetive positioning assignemnt to make your app interactive
+    
 
 
 
